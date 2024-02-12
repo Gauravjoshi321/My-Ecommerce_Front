b9b92e435ddbc3ccab3779c78644054aa46757f0
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-02-12 18:21:43 +0530

    now working...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import ProductList from './features/product-list/ProductList';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div>
+      <ProductList />
     </div>
   );
 }
