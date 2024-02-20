3f5e14af67cd8078eb87a715a58b6fc7adbf37ef
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-02-20 16:17:23 +0530

    theme added

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const products = [
   {
@@ -323,12 +323,12 @@ function Checkout() {
               </div>
               <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
               <div className="mt-6">
-                <a
-                  href="#"
+                <Link
+                  to="/pay"
                   className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                 >
                   Pay now
-                </a>
+                </Link>
               </div>
               <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                 <p>
