eed1afb74cffc894c19d1bd863879f9e1a948fa2
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-02-20 15:16:28 +0530

    half day

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -48,13 +48,13 @@ function Checkout() {
   const [open, setOpen] = useState(true)
 
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
         <div className="lg:col-span-3">
-          <form className="bg-white px-6">
+          <form className="bg-white px-6 mt-12 py-12">
             <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-              <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+              <h2 className="text-2xl font-semibold leading-7 text-gray-900">Personal Information</h2>
+              <p className="mt-2 text-sm leading-6 text-gray-500">Use a permanent address where you can receive mail.</p>
 
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="sm:col-span-3">
@@ -181,6 +181,17 @@ function Checkout() {
                 </div>
               </div>
             </div>
+            <div className="mt-8 flex items-center justify-end gap-x-6">
+              <button type="button" className="rounded-md bg-gray-100 px-6 py-[0.4rem] text-sm font-semibold leading-6 text-gray-900">
+                Reset
+              </button>
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Add address
+              </button>
+            </div>
 
             <div className="border-b border-gray-900/10 pb-12">
               <h2 className="text-base font-semibold leading-7 text-gray-900">Address</h2>
@@ -188,10 +199,15 @@ function Checkout() {
                 Choose from existing addresses
               </p>
 
-              <ul role="list" className="divide-y divide-gray-100">
+              <ul role="list" className="flex flex-col gap-2 mt-2">
                 {addresses.map((address) => (
-                  <li key={address.email} className="flex justify-between gap-x-6 py-5">
-                    <div className="flex min-w-0 gap-x-4">
+                  <li key={address.email} className="flex justify-between gap-x-6 px-5 py-5 border-solid border-2 border-gray-200">
+                    <input
+                      name="address"
+                      type="radio"
+                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                    <div className="min-w-0 flex-auto">
                       <div className="min-w-0 flex-auto">
                         <p className="text-sm font-semibold leading-6 text-gray-900">{address.name}</p>
                         <p className="mt-1 truncate text-xs leading-5 text-gray-500">{address.phone}</p>
@@ -240,25 +256,12 @@ function Checkout() {
                 </fieldset>
               </div>
             </div>
-
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-              <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Save
-              </button>
-
-            </div>
           </form >
         </div>
 
 
         <div className="lg:col-span-2">
-          <div className="mx-auto max-w-6xl mt-12 px-4 sm:px-6 lg:px-8 bg-white">
+          <div className="mx-auto max-w-6xl mt-12 px-0 sm:px-0 lg:px-0 bg-white">
 
             <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
               <h1 className="text-4xl font-semibold my-6 tracking-tight text-gray-900">Cart</h1>
@@ -324,7 +327,7 @@ function Checkout() {
                   href="#"
                   className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                 >
-                  Checkout
+                  Pay now
                 </a>
               </div>
               <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
