e8a04da34eb7fb0c5390d45f923de08f97560990
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-02-21 11:59:29 +0530

    new day new code

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -192,7 +192,7 @@ export default function ProductList() {
           </Dialog >
         </Transition.Root >
 
-        <main main main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" >
+        <main main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" >
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">All products</h1>
 
