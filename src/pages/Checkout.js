961a4b4e2d5d071847152dd86005ba6bcda280ce
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-03-04 19:33:36 +0530

    edited ecommerce

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -8,6 +8,7 @@ import { deleteItemFromCartAsync, selectItems, updateCartAsync, } from '../featu
 import { updateUserAsync } from '../features/auth/authSlice';
 import { createOrderAsync, selectCurrentOrder } from '../features/order/orderSlice';
 import { selectUserInfo } from '../features/user/userSlice';
+import { discountedPrice } from '../app/constants';
 
 
 function Checkout() {
@@ -24,7 +25,7 @@ function Checkout() {
   const currentOrder = useSelector(selectCurrentOrder);
 
   const totalAmount = items.reduce(
-    (amount, item) => item.price * item.quantity + amount,
+    (amount, item) => discountedPrice(item) * item.quantity + amount,
     0
   );
   const totalItems = items.reduce((total, item) => item.quantity + total, 0);
@@ -401,8 +402,7 @@ function Checkout() {
                               <h3>
                                 <a href={item.href}>{item.title}</a>
                               </h3>
-                              <p className="ml-4">${item.price}</p>
-                            </div>
+                              <p className="ml-4">${discountedPrice(item)}</p>                            </div>
                             <p className="mt-1 text-sm text-gray-500">
                               {item.brand}
                             </p>
